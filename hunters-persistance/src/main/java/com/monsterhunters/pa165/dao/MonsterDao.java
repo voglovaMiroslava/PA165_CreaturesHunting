920760521e13fd89debb4030b42e39da4e7534db
voglovaMiroslava <voglova.m@gmail.com>
2016-10-22 21:50:01 +0200

    implemented DAO for monster

--- hunters-persistance/src/main/java/com/monsterhunters/pa165/dao/MonsterDao.java
+++ hunters-persistance/src/main/java/com/monsterhunters/pa165/dao/MonsterDao.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.monsterhunters.pa165.dao;
+
+import com.monsterhunters.pa165.entity.Monster;
+import java.util.List;
 
 /**
  *
  * @author Miroslava Voglova
  */
 public interface MonsterDao {
+
+    /**
+     * Finds monster by it's id.
+     * @param id generated id of monster
+     * @return monster with given id
+     */
+    public Monster findById(Long id);
     
+    /**
+     * Saves monster to db.
+     * @param m monster which will be saved
+     */
+    public void create(Monster m);
+    
+    /**
+     * Updates monster in db.
+     * @param m monster which will be updated
+     */
+    public void update(Monster m);
+    
+    /**
+     * Removes monster from db.
+     * @param m monster to be removed
+     */
+    public void delete(Monster m);
+    
+    /**
+     * Returns all monsters that was saved in db.
+     * @return list of monsters from db
+     */
+    public List<Monster> findAll();
+    
+    /**
+     * Returns monster with given name
+     * @param name name of the monster we are looking for
+     * @return monster with given name
+     */
+    public Monster findByName(String name);
 }
--- hunters-persistance/src/main/java/com/monsterhunters/pa165/dao/MonsterDaoImpl.java
+++ hunters-persistance/src/main/java/com/monsterhunters/pa165/dao/MonsterDaoImpl.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.monsterhunters.pa165.dao;
+
+import com.monsterhunters.pa165.entity.Monster;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 /**
  *
- * @author voglovam
+ * @author Miroslava Voglova
  */
-public class MonsterDaoImpl {
-    
+@Transactional
+public class MonsterDaoImpl implements MonsterDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Monster findById(Long id) {
+        return em.find(Monster.class, id);
+    }
+
+    @Override
+    public void create(Monster m) {
+        em.persist(m);
+    }
+
+    @Override
+    public void update(Monster m) {
+        em.refresh(m);
+    }
+
+    @Override
+    public void delete(Monster m) {
+            em.remove(m);
+    }
+
+    @Override
+    public List<Monster> findAll() {
+        return em.createQuery("Select m from Monster m", Monster.class).getResultList();
+    }
+
+    @Override
+    public Monster findByName(String name) {
+        try {
+            return em.createQuery("Select m from Monster m where m.name = :name", Monster.class)
+                    .setParameter(":name", name)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 }
