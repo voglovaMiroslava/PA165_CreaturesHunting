e62a058980f9ad2c9610bf32d468f5f7f343d564
Jiří Vahala <snurkabill@gmail.com>
2016-10-30 21:29:39 +0100

    Refactored tests

--- hunters-persistance/src/test/java/com/monsterhunters/pa165/dao/UserDaoTest.java
+++ hunters-persistance/src/test/java/com/monsterhunters/pa165/dao/UserDaoTest.java
@@ -4,14 +4,17 @@ import com.monsterhunters.pa165.PersistenceSampleApplicationContext;
 import com.monsterhunters.pa165.dao.CommentDao;
 import com.monsterhunters.pa165.dao.UserDao;
 import com.monsterhunters.pa165.entity.User;
-import org.junit.Assert;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Created by Snurka on 10/30/2016.
@@ -21,8 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserDaoTest extends AbstractTestNGSpringContextTests {
 
+    @PersistenceContext
+    public EntityManager em;
+
     @Autowired
-    private UserDao userDao;
+    UserDao userDao;
 
     @Autowired
     private CommentDao commentDao;
