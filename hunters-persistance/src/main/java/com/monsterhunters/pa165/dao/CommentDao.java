fd41c8ed35e14c6fbac521027dbc31d84fa08044
Jiří Vahala <snurkabill@gmail.com>
2016-10-30 12:50:38 +0100

    Added commentDao, alternated user

--- hunters-persistance/src/main/java/com/monsterhunters/pa165/dao/CommentDao.java
+++ hunters-persistance/src/main/java/com/monsterhunters/pa165/dao/CommentDao.java
@@ -1,7 +1,25 @@
 package com.monsterhunters.pa165.dao;
 
+import com.monsterhunters.pa165.entity.Comment;
+import com.monsterhunters.pa165.entity.User;
+
+import java.util.List;
+
 /**
  * Created by Snurka on 10/30/2016.
  */
 public interface CommentDao {
+
+    Comment findById(Long id);
+
+    Comment findByUser(User user);
+
+    List<Comment> getAll();
+
+    void create(Comment comment);
+
+    void delete(Comment comment);
+
+    void update(Comment comment);
+
 }
