d06a6ddf023d90c4d3a1c7f8a0ab2758016bf859
Jiří Vahala <snurkabill@gmail.com>
2016-10-31 16:19:23 +0100

    Added user

---
+++ hunters-persistance/src/main/java/com/monsterhunters/pa165/entity/User.java
@@ -0,0 +1,111 @@
+package com.monsterhunters.pa165.entity;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+/**
+ * Created by Snurka on 10/30/2016.
+ */
+
+@Entity
+@Table(name = "MonsterUser")
+public class User {
+
+    @Id
+    @Column(nullable = false, length = 36)
+    @Type(type = "uuid-char")
+    @NotNull
+    private UUID id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @NotNull
+    @Column(nullable = false)
+    private boolean isAdmin;
+
+    public User(String nickname, String email, String passwordHash, boolean isAdmin) {
+        this.id = UUID.randomUUID();
+        this.nickname = nickname;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.isAdmin = isAdmin;
+    }
+
+    protected User() {
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User))  {
+            return false;
+        }
+        User user = (User) o;
+        if (!getNickname().equals(user.getNickname()))  {
+            return false;
+        }
+        return getEmail().equals(user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getNickname().hashCode();
+        result = 31 * result + getEmail().hashCode();
+        return result;
+    }
+}
