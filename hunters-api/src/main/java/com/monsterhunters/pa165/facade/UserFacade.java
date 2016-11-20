0f06a2420e2974415aa1a1d2724e7a7e58a65cdb
Jiří Vahala <snurkabill@gmail.com>
2016-11-20 17:47:50 +0100

    UserFacade api alternatio

--- hunters-api/src/main/java/com/monsterhunters/pa165/facade/UserFacade.java
+++ hunters-api/src/main/java/com/monsterhunters/pa165/facade/UserFacade.java
@@ -1,9 +1,11 @@
 package com.monsterhunters.pa165.facade;
 
+import com.monsterhunters.pa165.dto.UserAuthenticateDTO;
 import com.monsterhunters.pa165.dto.UserCreateDTO;
 import com.monsterhunters.pa165.dto.UserDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Snurka on 11/20/2016.
@@ -26,10 +28,40 @@ public interface UserFacade {
     UserDTO getUserById(Long id);
 
     /**
+     * Returns UserDTO with id
+     *
+     * @param email UserDTO email
+     * @return UserDTO object
+     */
+    UserDTO getUserByEmail(String email);
+
+    /**
+     * Returns UserDTO with id
+     *
+     * @param nickname UserDTO nickname
+     * @return UserDTO object
+     */
+    UserDTO getUserByNickname(String nickname);
+
+    /**
      * Create UserDTO object
      *
      * @param userCreateDTO
      * @return userDTO id
      */
-    Long createUser(UserCreateDTO userCreateDTO);
+    UserDTO registerUser(UserCreateDTO userCreateDTO);
+
+    /**
+     * Try to authenticate a user. Return true only if the hashed password matches the records.
+     */
+    boolean authenticateUser(UserAuthenticateDTO userAuthenticateDTO);
+
+    /**
+     * Check if the given user is admin.
+     */
+    boolean isAdmin(UserDTO userDTO);
+
+
+
+
 }
