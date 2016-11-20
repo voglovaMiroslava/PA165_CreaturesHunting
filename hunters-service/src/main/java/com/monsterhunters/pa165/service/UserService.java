ae32cef696a66ac9c72267310fbb9597623b99e7
Jiří Vahala <snurkabill@gmail.com>
2016-11-20 17:48:40 +0100

    User service implemented

---
+++ hunters-service/src/main/java/com/monsterhunters/pa165/service/UserService.java
@@ -0,0 +1,75 @@
+package com.monsterhunters.pa165.service;
+
+import com.monsterhunters.pa165.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Snurka on 11/20/2016.
+ */
+@Service
+public interface UserService {
+
+    /**
+     * Returns found User
+     *
+     * @param id UserId
+     * @return Found User
+     */
+    User findById(Long id);
+
+    /**
+     * Returns list of all users
+     *
+     * @return list of Users objects
+     */
+    List<User> findAll();
+
+    /**
+     * Returns found User
+     *
+     * @param nickname
+     * @return User
+     */
+    User findByNickname(String nickname);
+
+
+    /**
+     * Returns found User
+     *
+     * @param email
+     * @return User
+     */
+    User findByEmail(String email);
+
+    /**
+     * Registers new user
+     *
+     * @param user object
+     */
+    void registerUser(User user, String plainPassword);
+
+    /**
+     * Verifies new user
+     * @param user
+     * @param plainPassword
+     * @return
+     */
+    boolean authenticate(User user, String plainPassword);
+
+    /**
+     * Removes User
+     *
+     * @param user object
+     */
+    void remove(User user);
+
+
+    /**
+     * check if user is admin
+     * @param user
+     * @return
+     */
+    boolean isAdmin(User user);
+}
