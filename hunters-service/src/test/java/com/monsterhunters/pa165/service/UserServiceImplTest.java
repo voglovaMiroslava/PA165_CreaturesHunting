5b836b1c79a39bccb2a3196ad1c6d5235a04cd3b
Jiří Vahala <snurkabill@gmail.com>
2016-11-22 11:51:10 +0100

    Build is passing

--- hunters-service/src/test/java/com/monsterhunters/pa165/service/UserServiceImplTest.java
+++ hunters-service/src/test/java/com/monsterhunters/pa165/service/UserServiceImplTest.java
@@ -1,7 +1,11 @@
 package com.monsterhunters.pa165.service;
 
+import com.monsterhunters.pa165.dao.UserDao;
 import com.monsterhunters.pa165.entity.User;
 import com.monsterhunters.pa165.service.config.MappingConfiguration;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
@@ -11,12 +15,25 @@ import org.testng.annotations.Test;
  * Created by Snurka on 11/20/2016.
  */
 
-@ContextConfiguration(classes = MappingConfiguration.class)
+@ContextConfiguration(classes = {MappingConfiguration.class})
 public class UserServiceImplTest extends AbstractTestNGSpringContextTests {
+
+    @Mock
+    private UserDao userDao;
+
+    @Autowired
+    @InjectMocks
+    private UserService userService;
+
+//    @BeforeClass
+//    public void Setup() {
+//        MockitoAnnotations.initMocks(this);
+//
+//        when(userDao.create(any(User.class))).then();
+//    }
 
     @Test
     public void createUserTest() {
-        UserService userService = new UserServiceImpl();
         String nickname = "DrsnejTypek";
         String backupNickname = nickname; // Just kill me, now I don't believe anything. Is this needed? :D
         User user = new User();
