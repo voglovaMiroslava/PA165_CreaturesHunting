2d18c688e7e492ea714b19163f3283cc047fea8f
Jiří Vahala <snurkabill@gmail.com>
2016-11-22 11:32:44 +0100

    Added comment mapping for dozer

--- hunters-service/src/main/java/com/monsterhunters/pa165/service/config/EntityDTOMapping.java
+++ hunters-service/src/main/java/com/monsterhunters/pa165/service/config/EntityDTOMapping.java
@@ -1,15 +1,14 @@
 package com.monsterhunters.pa165.service.config;
 
+import com.monsterhunters.pa165.dto.CommentDTO;
 import com.monsterhunters.pa165.dto.LocationDTO;
 import com.monsterhunters.pa165.dto.UserDTO;
 import com.monsterhunters.pa165.dto.WeaponDTO;
+import com.monsterhunters.pa165.entity.Comment;
 import com.monsterhunters.pa165.entity.Location;
 import com.monsterhunters.pa165.entity.User;
 import com.monsterhunters.pa165.entity.Weapon;
 import org.dozer.loader.api.BeanMappingBuilder;
-
-//import com.monsterhunters.pa165.dto.WeaponDTO;
-//import com.monsterhunters.pa165.dto.MosnterDTO;
 
 // TODO: make mapping of monster...
 
@@ -25,6 +24,6 @@ public class EntityDTOMapping extends BeanMappingBuilder {
 //        mapping(Monster.class, MonsterDTO.class);
         mapping(Weapon.class, WeaponDTO.class);
         mapping(User.class, UserDTO.class);
-
+        mapping(Comment.class, CommentDTO.class);
     }
 }
