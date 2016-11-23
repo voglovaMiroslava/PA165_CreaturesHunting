03be1eceadcd96083f7ac62019a72e34b2936f6d
Gabriel B <g.babcan@gmail.com>
2016-11-23 21:58:41 +0100

    added exceptions to weapon classes

--- hunters-service/src/main/java/com/monsterhunters/pa165/service/WeaponServiceImpl.java
+++ hunters-service/src/main/java/com/monsterhunters/pa165/service/WeaponServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 /**
  * Implementation of the {@link WeaponService}.
- *
+ * <p>
  * Created by babcang
  *
  * @author Babcan G
@@ -26,62 +26,119 @@ public class WeaponServiceImpl implements WeaponService {
     private WeaponDao weaponDao;
 
     @Override
-    public Weapon createWeapon(Weapon w) {
-        weaponDao.create(w);
+    public Weapon createWeapon(Weapon w) throws HuntersServiceException {
+        if (w == null) {
+            throw new IllegalArgumentException("Weapon is null");
+        }
+        try {
+            weaponDao.create(w);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Something went wrong. " +
+                    "Weapon " + w.getName() + " cannot be created", e);
+        }
         return w;
     }
 
     @Override
-    public void deleteWeapon(Weapon w) {
-        weaponDao.delete(w);
+    public void deleteWeapon(Weapon w) throws HuntersServiceException {
+        if (w == null) {
+            throw new IllegalArgumentException("Weapon is null");
+        }
+        try {
+            weaponDao.delete(w);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Weapon " + w.getName() + " cannot be deleted", e);
+
+        }
     }
 
     @Override
-    public Weapon updateWeapon(Weapon w){
-        weaponDao.update(w);
+    public Weapon updateWeapon(Weapon w) throws HuntersServiceException {
+        if (w == null) {
+            throw new IllegalArgumentException("Weapon is null");
+        }
+        try {
+            weaponDao.update(w);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Weapon " + w.getName() + " cannot be updated", e);
+
+        }
         return w;
     }
 
     @Override
-    public void addComment(Weapon w, Comment c) {
+    public void addComment(Weapon w, Comment c) throws HuntersServiceException {
         if (w.getComments().contains(c)) {
             throw new HuntersServiceException("Same comment already exists for this weapon."
                     + " Weapon ID:" + w.getId() + " Comment ID:" + c.getId());
         }
-        w.addComment(c);
+        try {
+            w.addComment(c);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Cannot add comment " + c +
+                    " to weapon " + w, e);
+        }
     }
 
     @Override
-    public void removeComment(Weapon w, Comment c) {
-        w.removeComment(c);
+    public void removeComment(Weapon w, Comment c) throws HuntersServiceException {
+        try {
+            w.removeComment(c);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Cannot remove comment " + c +
+                    " from weapon " + w, e);
+        }
     }
 
     @Override
-    public void addEffectiveAgainst(Weapon w, MonsterType m) {
+    public void addEffectiveAgainst(Weapon w, MonsterType m) throws HuntersServiceException {
         if (w.getEffectiveAgainst().contains(m)) {
             throw new HuntersServiceException("Monster type is already at list of effective against."
                     + " Weapon ID:" + w.getId() + " Monster type:" + m.toString());
         }
-        w.addEffectiveAgainst(m);
+        try {
+            w.addEffectiveAgainst(m);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Cannot add monster type " + m +
+                    " to weapon " + w, e);
+        }
+
     }
 
     @Override
-    public void removeEffectiveAgainst(Weapon w, MonsterType m) {
-        w.removeEffectiveAgainst(m);
+    public void removeEffectiveAgainst(Weapon w, MonsterType m) throws HuntersServiceException {
+        try {
+            w.removeEffectiveAgainst(m);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Cannot remove monster type " + m +
+                    " from weapon " + w, e);
+        }
     }
 
     @Override
-    public List<Weapon> findAll() {
-        return weaponDao.findAll();
+    public List<Weapon> findAll() throws HuntersServiceException {
+        try {
+            return weaponDao.findAll();
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Cannot get list of weapons", e);
+        }
     }
 
     @Override
-    public Weapon findById(Long id) {
-        return weaponDao.findById(id);
+    public Weapon findById(Long id) throws HuntersServiceException {
+        try {
+            return weaponDao.findById(id);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Cannot get weapon with id:" + id, e);
+        }
     }
 
     @Override
-    public Weapon findByName(String wName) {
-        return weaponDao.findByName(wName);
+    public Weapon findByName(String wName) throws HuntersServiceException {
+        try {
+            return weaponDao.findByName(wName);
+        } catch (Throwable e) {
+            throw new HuntersServiceException("Cannot get weapon with name:" + wName, e);
+        }
     }
 }
