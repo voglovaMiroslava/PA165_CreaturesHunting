1c8f3d06209bd77a24179f9f5e3d2c84288af8d5
Gabriel B <g.babcan@gmail.com>
2016-11-23 13:33:02 +0100

    Add javadoc to facades and services of weapon and comment

--- hunters-service/src/main/java/com/monsterhunters/pa165/service/WeaponService.java
+++ hunters-service/src/main/java/com/monsterhunters/pa165/service/WeaponService.java
@@ -3,6 +3,7 @@ package com.monsterhunters.pa165.service;
 import com.monsterhunters.pa165.entity.Comment;
 import com.monsterhunters.pa165.entity.Weapon;
 import com.monsterhunters.pa165.enums.MonsterType;
+import com.monsterhunters.pa165.exceptions.HuntersServiceException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,66 +24,66 @@ public interface WeaponService {
      * @param w weapon object
      * @return created weapon
      */
-    Weapon createWeapon(Weapon w);
+    Weapon createWeapon(Weapon w) throws HuntersServiceException;
 
     /**
      * Delete weapon
      * @param w weapon object
      */
-    void deleteWeapon(Weapon w);
+    void deleteWeapon(Weapon w) throws HuntersServiceException;
 
     /**
      * Updates weapon
      * @param w weapon object to update
      * @return updated weapon object
      */
-    Weapon updateWeapon(Weapon w);
+    Weapon updateWeapon(Weapon w) throws HuntersServiceException;
 
     /**
      * Add comment to list of comments of weapon
      * @param w is weapon object
      * @param c is a comment object which will be added to list
      */
-    void addComment(Weapon w, Comment c);
+    void addComment(Weapon w, Comment c) throws HuntersServiceException;
 
     /**
      * Remove comment from list of comments of weapon
      * @param w is weapon object
      * @param c is a comment object to removed from list
      */
-    void removeComment(Weapon w, Comment c);
+    void removeComment(Weapon w, Comment c) throws HuntersServiceException;
 
     /**
      * Add monster type which can be injured by the weapon
      * @param w is weapon object
      * @param m is monster type
      */
-    void addEffectiveAgainst(Weapon w, MonsterType m);
+    void addEffectiveAgainst(Weapon w, MonsterType m) throws HuntersServiceException;
 
     /**
      * Remove monster type from effectiveAgainst list
      * @param w weapon object
      * @param m is monster type
      */
-    void removeEffectiveAgainst(Weapon w, MonsterType m);
+    void removeEffectiveAgainst(Weapon w, MonsterType m) throws HuntersServiceException;
 
     /**
      * Find all weapons
      * @return list of all weapons
      */
-    List<Weapon> findAll();
+    List<Weapon> findAll() throws HuntersServiceException;
 
     /**
      *  Find weapon by ID
      * @param id is id of a weapon
      * @return found weapon
      */
-    Weapon findById(Long id);
+    Weapon findById(Long id) throws HuntersServiceException;
 
     /**
      * Find weapon by its name
      * @param wName name of the weapon
      * @return found weapon
      */
-    Weapon findByName(String wName);
+    Weapon findByName(String wName) throws HuntersServiceException;
 }
