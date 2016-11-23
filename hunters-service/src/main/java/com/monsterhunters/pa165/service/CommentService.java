30517544283a246e64bc97cfd36e135166a6bce1
Gabriel B <g.babcan@gmail.com>
2016-11-23 22:03:28 +0100

    Edited Comment Service and ServiceImpl

--- hunters-service/src/main/java/com/monsterhunters/pa165/service/CommentService.java
+++ hunters-service/src/main/java/com/monsterhunters/pa165/service/CommentService.java
@@ -3,6 +3,7 @@ package com.monsterhunters.pa165.service;
 import java.util.*;
 import com.monsterhunters.pa165.entity.Comment;
 import com.monsterhunters.pa165.entity.User;
+import com.monsterhunters.pa165.exceptions.HuntersServiceException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,32 +22,32 @@ public interface CommentService {
      * @param c is comment which will be created
      * @return created comment
      */
-    Comment createComment(Comment c);
+    Comment createComment(Comment c) throws HuntersServiceException;
 
     /** Delete specified Comment
      *
      * @param c is comment which should be deleted
      */
-    void deleteComment(Comment c);
+    void deleteComment(Comment c) throws HuntersServiceException;
 
     /** Method find comment with specified id
      *
      * @param id is id of comment
      * @return found comment
      */
-    Comment findById(Long id);
+    Comment findById(Long id) throws HuntersServiceException;
 
     /** Method find list of all comments
      *
      * @return list of comments
      */
-    List<Comment> findAll();
+    List<Comment> findAll() throws HuntersServiceException;
 
     /** Method find comment by specified User
      *
      * @param user is user who comments want to find
      * @return list of comments
      */
-    List<Comment> findByUser(User user);
+    List<Comment> findByUser(User user) throws HuntersServiceException;
 
 }
