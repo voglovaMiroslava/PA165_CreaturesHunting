5be06851e183646f7790189abc120d994256d016
Jiří Vahala <snurkabill@gmail.com>
2016-11-24 11:00:18 +0100

    register user facade tests

--- hunters-service/src/test/java/com/monsterhunters/pa165/facade/UserFacadeTest.java
+++ hunters-service/src/test/java/com/monsterhunters/pa165/facade/UserFacadeTest.java
@@ -1,29 +1,27 @@
 package com.monsterhunters.pa165.facade;
 
-import com.monsterhunters.pa165.service.MappingService;
-import com.monsterhunters.pa165.service.UserService;
+import com.monsterhunters.pa165.dto.UserCreateDTO;
+import com.monsterhunters.pa165.dto.UserDTO;
+import com.monsterhunters.pa165.exceptions.user.EmailAlreadyExistsException;
+import com.monsterhunters.pa165.exceptions.user.NicknameAlreadyExistsException;
 import com.monsterhunters.pa165.service.config.MappingConfiguration;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Created by Snurka on 11/23/2016.
  */
 @ContextConfiguration(classes = {MappingConfiguration.class})
-public class UserFacadeTest {
+public class UserFacadeTest extends AbstractTransactionalTestNGSpringContextTests {
 
-    // TODO: Finish me :) 
-
-    @Mock
-    private MappingService mappingService;
-
-    @Mock
-    private UserService userService;
+    @Autowired
+    private UserFacade userFacade;
 
     @Test
     public void getUserByIdTest() {
-
     }
 
     @Test
@@ -38,7 +36,51 @@ public class UserFacadeTest {
 
     @Test
     public void registerUserTest() {
+        UserCreateDTO userCreateDTO = new UserCreateDTO();
+        userCreateDTO.setEmail("asdf@asdf.com");
+        userCreateDTO.setNickname("DrsnejTypek");
+        userCreateDTO.setPlainPassword("userPassword");
+        UserDTO userDTO = userFacade.registerUser(userCreateDTO);
+        Assert.assertEquals(userDTO.getEmail(), userCreateDTO.getEmail());
+        Assert.assertEquals(userDTO.getNickname(), userCreateDTO.getNickname());
+        Assert.assertEquals(userDTO.getId(), new Long(1));
 
+        UserCreateDTO userCreateDTO2 = new UserCreateDTO();
+        userCreateDTO2.setEmail("aaa@aaa.com");
+        userCreateDTO2.setNickname("Huhuuuu");
+        userCreateDTO2.setPlainPassword("userPassword");
+        UserDTO userDTO2 = userFacade.registerUser(userCreateDTO2);
+        Assert.assertEquals(userDTO2.getEmail(), userCreateDTO2.getEmail());
+        Assert.assertEquals(userDTO2.getNickname(), userCreateDTO2.getNickname());
+        Assert.assertEquals(userDTO2.getId(), new Long(2));
+    }
+
+    @Test(expectedExceptions = EmailAlreadyExistsException.class)
+    public void registerAlreadyExistingEmailTest() {
+        UserCreateDTO userCreateDTO = new UserCreateDTO();
+        userCreateDTO.setEmail("asdf@asdf.com");
+        userCreateDTO.setNickname("DrsnejTypek");
+        userCreateDTO.setPlainPassword("userPassword");
+        UserCreateDTO userCreateDTO2 = new UserCreateDTO();
+        userCreateDTO2.setEmail("asdf@asdf.com");
+        userCreateDTO2.setNickname("Huhuuuu");
+        userCreateDTO2.setPlainPassword("userPassword");
+        userFacade.registerUser(userCreateDTO);
+        userFacade.registerUser(userCreateDTO2);
+    }
+
+    @Test(expectedExceptions = NicknameAlreadyExistsException.class)
+    public void registerAlreadyExistingNicknameTest() {
+        UserCreateDTO userCreateDTO = new UserCreateDTO();
+        userCreateDTO.setEmail("asdf@asdf.com");
+        userCreateDTO.setNickname("DrsnejTypek");
+        userCreateDTO.setPlainPassword("userPassword");
+        UserCreateDTO userCreateDTO2 = new UserCreateDTO();
+        userCreateDTO2.setEmail("aaa@aaa.com");
+        userCreateDTO2.setNickname("DrsnejTypek");
+        userCreateDTO2.setPlainPassword("userPassword");
+        userFacade.registerUser(userCreateDTO);
+        userFacade.registerUser(userCreateDTO2);
     }
 
     @Test
