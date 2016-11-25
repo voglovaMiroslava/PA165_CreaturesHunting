441fb562ce6dcd2cb45efebc50c061ae6c761011
Jiří Vahala <snurkabill@gmail.com>
2016-11-25 17:49:12 +0100

    Added userDTO checks

--- hunters-api/src/main/java/com/monsterhunters/pa165/dto/UserCreateDTO.java
+++ hunters-api/src/main/java/com/monsterhunters/pa165/dto/UserCreateDTO.java
@@ -1,10 +1,27 @@
 package com.monsterhunters.pa165.dto;
 
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UserCreateDTO {
 
+    @NotNull
+    @Size(min = 5, max = 30)
     private String nickname;
+
+    @NotNull
+    @Email(message="Please provide a valid email address")
+    @Pattern(regexp=".+@.+\\..+", message="Please provide a valid email address")
     private String email;
+
+    @NotNull
+    @Size(min = 7, max = 20)
+    // just overkill for this app
+    //@Pattern(regexp="((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})", message = "Please provide")
     private String plainPassword;
 
     public String getNickname() {
