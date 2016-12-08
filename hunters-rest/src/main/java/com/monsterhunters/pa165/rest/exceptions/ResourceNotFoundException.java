88fd576a5d167b0aee7e3b3a24b83d28bef4c3e6
Gabriel B <g.babcan@gmail.com>
2016-12-08 23:50:20 +0100

    Rest exceptions and webapp context

---
+++ hunters-rest/src/main/java/com/monsterhunters/pa165/rest/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.monsterhunters.pa165.rest.exceptions;
+
+/**
+ * Created by babcang
+ *
+ * @author Babcan G
+ */
+
+//@ResponseStatus(value = HttpStatus.NOT_FOUND, reason="The requested resource was not found")
+public class ResourceNotFoundException extends RuntimeException {
+}
